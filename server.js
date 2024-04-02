d061aead926733c0ee457842bdef6e8df5129bdb
sameed02 <sameedahmad3@gmail.com>
2024-04-02 17:55:40 +0530

    setup ATLAST,Compass and connected to Database using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-
+const dotenv = require("dotenv").config({ path: "./config.env" });
+const mongoose = require("mongoose");
 const { app } = require("./app");
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`app is listening on port 3000`));
+const DB = process.env.DATABASE.replace("<password>", process.env.PASSWORD);
+
+//  { useNewUrlParser: true } is used to parse the connection string and prevent the deprecation warning that was introduced in Mongoose version 5
+async function connectDB() {
+  try {
+    await mongoose.connect(DB);
+    console.log("DB connection successfull");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connectDB();
+
+app.listen(process.env.PORT, () =>
+  console.log(`app is listening on port 3000`)
+);
