ede3912122ed83a65270321d2dc608c3613716fd
sameed02 <sameedahmad3@gmail.com>
2024-04-08 20:16:32 +0530

    updated tourSchema i.e dates, duration, difficulty, ratings and more...

--- models/tourModels/tourSchema.js
+++ models/tourModels/tourSchema.js
@@ -1,14 +1,55 @@
 const mongoose = require("mongoose");
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A tour must have a name"],
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+      unique: true,
+      trim: true,
+    },
+
+    rating: { type: Number, default: 4.5 },
+
+    price: { type: Number, required: [true, "A tour must have a price"] },
+
+    duration: { type: Number, required: [true, "A tour must have a duration"] },
+
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a group size"],
+    },
+
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty"],
+    },
+
+    ratingsAverage: { type: Number, default: 4.5 },
+
+    ratingsQuantity: { type: Number, default: 0 },
+
+    priceDiscount: Number,
+
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a summary"],
+    },
+
+    description: { type: String, trim: true },
+
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover-image"],
+    },
+
+    images: [String],
+
+    startDates: [Date],
   },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, "A tour must have a price"] },
-});
+  { timestamps: true }
+);
 
 const Tour = mongoose.model("Tour", tourSchema);
 
