9f4f2c8b222326aa2a82d3016a7cffeef543ca10
sameed02 <sameedahmad3@gmail.com>
2024-04-09 18:34:51 +0530

    added script for importing or delete dev data using process.args, refer import-dev-data.s in dev data folder. update package.json to import or delete data via script.

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const dotenv = require("dotenv").config({ path: `./config.env` });
+const mongoose = require("mongoose");
+
+const { readFileData } = require("./../../file");
+const { Tour } = require("./../../models/tourModels/tourSchema");
+
+const DB = process.env.DATABASE.replace("<password>", process.env.PASSWORD);
+
+async function connectDB() {
+  try {
+    await mongoose.connect(DB);
+    console.log("DB connection successfull");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connectDB();
+
+// Read-File
+
+const tours = readFileData(`${__dirname}/tours-simple.json`);
+
+// Import data into DB
+
+async function importData() {
+  try {
+    await Tour.create(tours);
+    console.log("Data successfully loaded!");
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+}
+
+// Delete all data from collection
+
+async function deleteData() {
+  try {
+    await Tour.deleteMany();
+    console.log("Data successfully deleted!");
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+}
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
+
+console.log(process.argv);
