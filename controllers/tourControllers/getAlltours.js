792b1a1357eb321a1eb88209b42940f64457e112
sameed02 <sameedahmad3@gmail.com>
2024-04-10 21:30:34 +0530

    added api filtering

--- controllers/tourControllers/getAlltours.js
+++ controllers/tourControllers/getAlltours.js
@@ -1,8 +1,30 @@
 const { Tour } = require("./../../models/tourModels/tourSchema");
 
+/* 
+ In this below code, we're grabbing filtering information from `req.query`. Initially, it might seem easy to just plug this info into `Tour.find(req.query)`. But there's a hitch: if we want to do things like sorting or limiting results, we can't because `Tour.find(req.query)` will treat those parameters as part of the filter. Instead, we need to separate out these extra features and handle them individually.
+
+ alternate way of doing exluding page,sort etc...
+
+  const queryObj = {...req.query}
+  const excludeQueries = ["page", "sort", "limit", "fields"];
+  for (const key of excludeQueries) {
+    delete queryObj[key];
+  } 
+ */
+
 async function getAllTours(req, res) {
+  const { page, sort, limit, fields, ...queryObj } = req.query;
+  /* 
+Tour.find(queryObj) creates a query object.
+const query = Tour.find(queryObj); stores this query object in the variable query.
+await query; pauses execution and waits for the promise returned by query to resolve, which happens when the query is executed and the results are returned.
+Once the promise is resolved, the result is assigned to the variable tours, allowing you to work with the data retrieved from the database.
+
+we're doing all this to make chain other features like pagination, sorting, limits etc
+  */
   try {
-    const tours = await Tour.find();
+    const query = Tour.find(queryObj);
+    const tours = await query;
     res.status(200).json({
       status: "success",
       result: tours.length,
