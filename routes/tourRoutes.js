06c5e6ec69157bfb2269ea88eb5e3b4b88b03497
sameed02 <sameedahmad3@gmail.com>
2024-04-19 19:46:20 +0530

    added monthly-plan controller using aggregation pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,11 +7,14 @@ const { updateTour } = require("../controllers/tourControllers/updateTour");
 const { deleteTour } = require("../controllers/tourControllers/deleteTour");
 const { topTours } = require("../controllers/tourControllers/topTours");
 const { getTourStats } = require("../controllers/tourControllers/getTourStats");
+const {
+  getMonthlyPlan,
+} = require("../controllers/tourControllers/getMonthlyPlan");
 
 const tourRouter = express.Router();
 
 tourRouter.route("/tour-stats").get(getTourStats);
-
+tourRouter.route("/monthly-plan/:year").get(getMonthlyPlan);
 tourRouter.route("/top-5-tours").get(topTours, getAllTours);
 
 tourRouter.route("/").get(getAllTours).post(createTour);
