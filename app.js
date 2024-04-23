27c17865024c1cf5bce155e10b3a5be3c10eb37f
sameed02 <sameedahmad3@gmail.com>
2024-04-23 21:46:43 +0530

    added error middleware for undefined route in app.js

--- app.js
+++ app.js
@@ -12,5 +12,10 @@ app.use(express.json());
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
-
+app.use((req, res, next) => {
+  res.status(404).send({
+    status: "fail",
+    message: `can't find ${req.originalUrl} on the server`,
+  });
+});
 module.exports = { app };
