ba9e81206ffb84610d6edaf163b04e37ac15824e
sameed02 <sameedahmad3@gmail.com>
2024-04-30 20:25:03 +0530

    updated sign up using bcrypt

--- controllers/authController/signUp.js
+++ controllers/authController/signUp.js
@@ -1,6 +1,6 @@
 const { User } = require("../../models/userModels/userModel");
 const { AppError } = require("./../../utils/appError");
-const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 async function signUp(req, res, next) {
   try {
--- models/userModels/userModel.js
+++ models/userModels/userModel.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+const { AppError } = require("./../../utils/appError");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,12 +31,34 @@ const userSchema = new mongoose.Schema({
     trim: true,
     required: [true, "Please confirm your password"],
     validate: {
+      // This validator only works on CREATE and SAVE!!!
       validator: function (value) {
         return value === this.password;
       },
       message: "Passwords do not match",
     },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      next();
+    } else {
+      const salt = await bcrypt.genSalt(12);
+
+      // Hash the password along with the new salt
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+
+      // Replace the plain password with the hashed password
+      this.password = hashedPassword;
+      this.passwordConfirm = undefined;
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+    next(new AppError(err.message, 400));
+  }
 });
 
 const User = mongoose.model("User", userSchema);
