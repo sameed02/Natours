f772e69b46cf734c810ebadc0e6b805007906dc5
sameed02 <sameedahmad3@gmail.com>
2024-05-11 22:05:54 +0530

    added security HTTP headers using Helmet package. restructured code in app.js. and i have updated notes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const { AppError } = require("./utils/appError");
 const { globalErrorHandler } = require("./globalErrorHandler");
@@ -8,29 +9,39 @@ const { globalErrorHandler } = require("./globalErrorHandler");
 const { tourRouter } = require("./routes/tourRoutes");
 const { userRouter } = require("./routes/userRoutes");
 
+/* ------------------- APP ------------------- */
+
 const app = express();
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 /* Global Middlewares */
 
+// setting up security HTTP headers
+app.use(helmet());
+
+// limit no of api requests
 const limiter = rateLimit({
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 1 hour).
   windowMs: 60 * 60 * 1000, // 1 hour
   message: "Too many requests from this IP, please try again in an hour!",
 });
-
 app.use("/api", limiter);
 
-app.use(express.json());
+// Body parser, reading data from body & putting into req.body
+app.use(express.json({ limit: "10kb" }));
 
+//routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+// error middleware for accessing undefined routes
 app.use((req, res, next) => {
   const err = new AppError(`can't find ${req.originalUrl} on the server`, 404);
   next(err);
 });
 
+// globalerror handling middleware
 app.use(globalErrorHandler);
+
 module.exports = { app };
