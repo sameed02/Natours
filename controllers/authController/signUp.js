3263a6633f5d73566ccfd295230b8bd501b9f609
sameed02 <sameedahmad3@gmail.com>
2024-06-26 19:23:44 +0530

    updated signUp, now users get welcome email on successfull signup

--- controllers/authController/signUp.js
+++ controllers/authController/signUp.js
@@ -1,4 +1,5 @@
 const { User } = require("../../models/userModels/userModel");
+const { sendEmail } = require("../../utils/email");
 const { AppError } = require("./../../utils/appError");
 const { createSendToken } = require("./createSendToken");
 
@@ -13,6 +14,7 @@ async function signUp(req, res, next) {
       role,
       ...other
     } = req.body;
+
     const newUser = await User.create({
       name,
       email,
@@ -23,6 +25,12 @@ async function signUp(req, res, next) {
     });
 
     createSendToken(res, newUser, 201, { user: newUser });
+
+    await sendEmail({
+      email,
+      subject: "Natours Signup",
+      message: `Welcome to Natours ${name}, at Natours We want to help you get closer to nature in its most natural and raw state. When you join us, we will guide you to explore untouched places every day.`,
+    });
   } catch (err) {
     if (err.code === 11000) {
       const duplcateField = Object.keys(err.keyValue)[0];
